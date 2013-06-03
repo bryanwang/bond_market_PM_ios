4a83ca1e55835bc79e8c33ae50381953b39fe64f
bruce yang <meteor1211@gmail.com>
2013-06-03 09:29:34 +0800

    add quick dialog

---
+++ Support/QuickDialog/quickdialog/QPickerElement.h
@@ -0,0 +1,24 @@
+#import <Foundation/Foundation.h>
+#import "QEntryElement.h"
+#import "QPickerValueParser.h"
+
+@interface QPickerElement : QEntryElement
+{
+@protected
+    id<QPickerValueParser> _valueParser;
+}
+
+@property (nonatomic, strong) id<QPickerValueParser> valueParser;
+
+@property (nonatomic, strong) NSArray *items;
+@property (nonatomic, readonly) NSArray *selectedIndexes;
+
+- (QPickerElement *)initWithTitle:(NSString *)title items:(NSArray *)items value:(id)value;
+
+- (void)reloadAllComponents;
+- (void)reloadComponent:(NSInteger)index;
+
+//add by bruce yang
+- (void)selectRow:(NSInteger)row inComponent:(NSInteger)component animated:(BOOL)animated;  // scrolls the specified row to center.
+
+@end
