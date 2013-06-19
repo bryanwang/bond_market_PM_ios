72473d0cb046e35470bc22ffb9a92697b647b4e4
yuxin <bob.yuxinyang@gmail.com>
2013-06-19 15:26:49 +0800

    add my platform project VC

---
+++ bond_market_PM_iphone/MyPlatformProjectViewController.m
@@ -0,0 +1,38 @@
+//
+//  MyPlatformProjectViewController.m
+//  bond_market_PM_iphone
+//
+//  Created by YANG Yuxin on 13-6-19.
+//  Copyright (c) 2013年 pyrating. All rights reserved.
+//
+
+#import "MyPlatformProjectViewController.h"
+
+@interface MyPlatformProjectViewController ()
+
+@end
+
+@implementation MyPlatformProjectViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
